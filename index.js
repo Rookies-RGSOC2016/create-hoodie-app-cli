74baa6aa38747c51c48aa3d146d96b1b8c9c18ca
titay2 <tehetenamas@gmail.com>
2016-09-23 19:30:01 +0300

    npm start noe working

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
-console.log('hello world')
-var exec = require('child_process').exec;
-var    child;
 
-// get exec from that utily, give the command you want to run as it's first argument
- child = exec('npm install ffi',
- function (error, stdout, stderr) {
- // these are the result, somehow, it's devided to when there's error, or output etc.
-     console.log('stdout: ' + stdout);
-     console.log('stderr: ' + stderr);
-     if (error !== null) {
-          console.log('exec error: ' + error);
-     }
+var fs = require('fs')
+var child = require('child_process').exec;
+var exec = require('child_process').exec;
+var dir = './tmp';
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+  fs.writeFile("tmp/package.json",JSON.stringify({
+    name: "tmp",
+    script: "tmp",
+    version: "1.0.0",
+    description: "",
+    main: "index.js",
+    keywords: [],
+    author: "",
+    license: "ISC",
+    scripts: {
+      start: "hoodie",
+      test: "echo \"Error: no test specified\" && exit 1"
+  }
+  },
+    null,2), function(err) {
+      if(err) {
+          console.log("error"+err)
+      } else {
+          console.log("The file was saved!");
+      }
+  })
+
+ exec('npm install hoodie --save', {
+   cwd: 'tmp'
+ }, function(error, stdout, stderr) {
+   console.log('stdout: ' + stdout);
+   console.log('stderr: ' + stderr);
+   if (error !== null) {
+        console.log('exec error: ' + error);
+   }
  });
+
+// child('npm start', {
+//   cwd: 'tmp'
+// }, function(error, stdout, stderr) {
+//   console.log('stdout: ' + stdout);
+//   console.log('stderr: ' + stderr);
+//   if (error !== null) {
+//        console.log('exec error: ' + error);
+//   }
+// });
+//
+
+console.log('hello world')
